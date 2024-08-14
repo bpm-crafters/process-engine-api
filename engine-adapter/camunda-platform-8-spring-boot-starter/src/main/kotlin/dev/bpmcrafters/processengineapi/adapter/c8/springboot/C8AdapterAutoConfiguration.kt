a2a2d92f95bd474530f776cb4b542467c0bddfd5
Simon Zambrovski <simon.zambrovski@holisticon.de>
2024-08-14 14:50:16 +0200

    made C8 config conditional, fix #58

--- engine-adapter/camunda-platform-8-spring-boot-starter/src/main/kotlin/dev/bpmcrafters/processengineapi/adapter/c8/springboot/C8AdapterAutoConfiguration.kt
+++ engine-adapter/camunda-platform-8-spring-boot-starter/src/main/kotlin/dev/bpmcrafters/processengineapi/adapter/c8/springboot/C8AdapterAutoConfiguration.kt
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -30,6 +31,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
   CamundaAutoConfiguration::class
 )
 @EnableConfigurationProperties(value = [C8AdapterProperties::class])
+@ConditionalOnProperty(prefix = C8AdapterProperties.DEFAULT_PREFIX, name = ["enabled"], havingValue = "true", matchIfMissing = true)
 class C8AdapterAutoConfiguration {
 
   @Bean("c8-start-process-api")
