66bf3ac7994b3ab1102729ce4239bd33c3d1a0e8
Simon Zambrovski <simon.zambrovski@holisticon.de>
2025-03-31 12:29:24 +0200

    perform repository separation

--- engine-adapter/camunda-platform-7-embedded-core/src/main/kotlin/dev/bpmcrafters/processengineapi/adapter/c7/embedded/task/delivery/ExternalServiceTaskDelivery.kt
+++
@@ -1,6 +0,0 @@
-package dev.bpmcrafters.processengineapi.adapter.c7.embedded.task.delivery
-
-/**
- * Common interface for external service task delivery.
- */
-interface ExternalServiceTaskDelivery
--- engine-adapter/camunda-platform-8-spring-boot-starter/src/test/kotlin/dev/bpmcrafters/processengineapi/adapter/c8/C8TestApplication.kt
+++
@@ -1,9 +0,0 @@
-package dev.bpmcrafters.processengineapi.adapter.c8
-
-import com.tngtech.jgiven.integration.spring.EnableJGiven
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-
-@EnableJGiven
-@SpringBootApplication
-class C8TestApplication
