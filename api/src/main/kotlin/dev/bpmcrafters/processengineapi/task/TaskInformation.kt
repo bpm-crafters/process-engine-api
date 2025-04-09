f0c2f3d7131bbbf51b56aee8aabd247cf9745d97
Simon Zambrovski <simon.zambrovski@holisticon.de>
2025-04-09 20:08:13 +0200

    Support change detection of modification reasons, fix #223, fix #226

--- api/src/main/kotlin/dev/bpmcrafters/processengineapi/task/TaskInformation.kt
+++ api/src/main/kotlin/dev/bpmcrafters/processengineapi/task/TaskInformation.kt
@@ -13,4 +13,31 @@ data class TaskInformation(
    * Additional metadata about the task.
    */
   val meta: Map<String, String>
-)
+) {
+  companion object {
+    const val REASON = "reason"
+
+    const val CREATE = "create"
+    const val ASSIGN = "assign"
+    const val UPDATE = "update"
+    const val COMPLETE = "complete"
+    const val DELETE = "delete"
+  }
+
+  /**
+   * Creates a new task information with specified reason.
+   * @param reason reason to set.
+   * @return task information.
+   */
+  fun withReason(reason: String): TaskInformation {
+    return this.copy(meta = meta + (REASON to reason))
+  }
+
+  /**
+   * Cleans up th reason, if previously available.
+   * @return task information without reason set.
+   */
+  fun cleanupReason(): TaskInformation {
+    return this.copy(meta = meta.filterNot { it.key == REASON })
+  }
+}
